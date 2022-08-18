b85b4cbb0bb0e549b329ac8874e5aabce6a8a5b3
Himanshu Ranjan <himanshur@lambdatest.com>
2022-08-18 14:22:01 +0530

    timeout

--- tests/example.test.js
+++ tests/example.test.js
@@ -35,7 +35,7 @@ describe('Blog - kodziak.com/blog/', () => {
   test('should work -1', async () => {
     await page.goto('https://www.example.com');
     expect(await page.title()).toBe('Example Domain');
-  }, 300)
+  }, 3000)
 
   // test('should work -2 ', async () => {
   //   await page.goto('https://www.example.com');
@@ -59,7 +59,7 @@ describe('Blog - kodziak.com/blog/', () => {
   test('should work', async () => {
     await page.goto('https://www.example.com');
     expect(await page.title()).toBe('Example Domain');
-  }, 300)
+  }, 3000)
 
   // test('click on blog post should redirect to article', async () => {
   //   // Go to first blog post, there we're waiting to resolve all promises from array
